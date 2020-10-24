5b6e893680bda48c9232dbc028a7f446ffcba19f
Vadim <66322242+vadimqq@users.noreply.github.com>
2020-10-24 22:53:35 +0400

    import geolocation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import VueGeolocation from 'vue-browser-geolocation'
+
+Vue.use(VueGeolocation);
 
 Vue.config.productionTip = false
 
